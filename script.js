bf1aaa8dd70837a1e3fb87d53c75a0d16882a66a
Javier Preciado <jpreciado85@gmail.com>
2025-02-08 23:14:52 -0500

    animate.css en proyectos

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
+
+
+// Validación y envío del formulario de contacto
 document.getElementById('contacto').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
+    const nombre = document.getElementById('nombre').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const mensaje = document.getElementById('mensaje').value.trim();
 
     // Validación de campos vacíos
     if (!nombre || !email || !mensaje) {
         M.toast({html: 'Por favor, llena todos los campos.'});
-        return; // Detener ejecución si algún campo está vacío
+        return;
     }
 
     // Validación del correo electrónico
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailPattern.test(email)) {
         M.toast({html: 'Por favor, ingresa un correo electrónico válido.'});
         return;
     }
 
-    // Enviar formulario (simulando el envío)
+    // Simular el envío del formulario
     M.toast({html: 'Formulario enviado con éxito!'});
 
-    // Limpiar los campos del formulario
-    document.getElementById('contacto').reset();
+    // Limpiar el formulario
+    this.reset();
 });
+
+// SWEETALERT PARA IMÁGENES DE PROYECTOS
+
+function agregarEventoImagenes() {
+    document.querySelectorAll(".card img").forEach((img) => {
+        // Verifica si la imagen ya tiene un evento asignado
+        if (!img.dataset.listener) {
+            img.dataset.listener = "true"; // Marca la imagen como escuchada
+            
+            img.addEventListener("click", function () {
+                
+                Swal.fire({
+                    imageUrl: this.src,
+                    imageAlt: "Vista del proyecto",
+                    showCloseButton: true,
+                    showConfirmButton: false,
+                    customClass: {
+                        popup: "swal-wide"
+                    },
+                    showClass: {
+                        popup: 'animate__animated animate__fadeIn'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__zoomOut'
+                    },
+                });
+            });
+        }
+    });
+}
+
+// Ejecutar la función cuando se cargue el DOM
+document.addEventListener("DOMContentLoaded", agregarEventoImagenes);
+
+
+
+
